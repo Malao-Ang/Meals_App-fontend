373ac419578a5fffeafd2d5ef14a3d134f181719
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-04-18 15:25:14 +0700

    🔖 add dependent provider

--- lib/providers/filters_provider.dart
+++ lib/providers/filters_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/providers/meals_provider.dart';
 
 // import 'package:meals_app/filter.dart';
 enum Filter { glutenFree, lactoseFree, vegetarian, vegan }
@@ -27,3 +28,23 @@ class FlutterNotifier extends StateNotifier<Map<Filter, bool>> {
 final filtersProvider =
     StateNotifierProvider<FlutterNotifier, Map<Filter, bool>>(
         (ref) => FlutterNotifier());
+
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeFilters = ref.watch(filtersProvider);
+  return meals.where((meal) {
+    if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+    if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      return false;
+    }
+    if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+    if (activeFilters[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+    return true;
+  }).toList();
+});
--- lib/tabs.dart
+++ lib/tabs.dart
@@ -59,21 +59,8 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
   Widget build(BuildContext context) {
     final meals = ref.watch(mealsProvider);
     final activeFilters = ref.watch(filtersProvider);
-    final availableMeaks = meals.where((meal) {
-      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-        return false;
-      }
-      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-        return false;
-      }
-      if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-        return false;
-      }
-      if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-        return false;
-      }
-      return true;
-    }).toList();
+    final availableMeaks = ref.watch(filteredMealsProvider);
+
     var activePageTitle = 'Categories';
     Widget activePage = CategoriesScreen(
       // onToggleFavorites: _toggleMealFavorityStatus,
