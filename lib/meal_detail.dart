6a2fef187894c9ef5002dc3f7fad1d154b658292
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-04-19 13:01:01 +0700

    ✨ cofigulate animation

--- lib/meal_detail.dart
+++ lib/meal_detail.dart
@@ -30,7 +30,19 @@ class MealDetailsScreen extends ConsumerWidget {
                     content: Text(
                         wasAdd ? 'Meal added as favorite' : 'Meal remove.')));
               },
-              icon: Icon(isFavorite ? Icons.star : Icons.star_border))
+              icon: AnimatedSwitcher(
+                duration: Duration(milliseconds: 300),
+                transitionBuilder: (child, animation) {
+                  return RotationTransition(
+                    turns: Tween(begin: 0.8, end: 1.0).animate(animation),
+                    child: child,
+                  );
+                },
+                child: Icon(
+                  isFavorite ? Icons.star : Icons.star_border,
+                  key: ValueKey(isFavorite),
+                ),
+              ))
         ],
       ),
       body: SingleChildScrollView(
